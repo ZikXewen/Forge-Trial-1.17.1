3b49e51961a3c8785f57d9374995b000665ba71d
ZikXewen <datasup12@gmail.com>
2021-08-18 00:06:41 +0700

    Creative Tab and Porting Resources

--- src/main/java/com/dezilaicosed/pay_ging/PayGing.java
+++ src/main/java/com/dezilaicosed/pay_ging/PayGing.java
@@ -1,5 +1,7 @@
 package com.dezilaicosed.pay_ging;
 
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -9,14 +11,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import com.dezilaicosed.pay_ging.core.init.BlockInit;
 import com.dezilaicosed.pay_ging.core.init.ItemInit;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 @Mod(PayGing.MOD_ID)
 public class PayGing {
-    public static final Logger LOGGER = LogManager.getLogger();
-
     public static final String MOD_ID = "pay_ging";
+    public static final PayGingTab TAB = new PayGingTab("pay_ging_all");
 
     public PayGing() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -30,5 +28,16 @@ public class PayGing {
 
     private void setup(final FMLCommonSetupEvent event) {
 
+    }
+
+    public static class PayGingTab extends CreativeModeTab {
+        public PayGingTab(String label) {
+            super(label);
+        }
+
+        @Override
+        public ItemStack makeIcon() {
+            return ItemInit.PAY_GING.get().getDefaultInstance();
+        }
     }
 }
--- src/main/java/com/dezilaicosed/pay_ging/core/init/ItemInit.java
+++ src/main/java/com/dezilaicosed/pay_ging/core/init/ItemInit.java
@@ -3,19 +3,18 @@ package com.dezilaicosed.pay_ging.core.init;
 import com.dezilaicosed.pay_ging.PayGing;
 
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ItemInit {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, PayGing.MOD_ID);
-    public static final RegistryObject<Item> PAY_GING = ITEMS.register("pay_ging",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+        public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+                        PayGing.MOD_ID);
+        public static final RegistryObject<Item> PAY_GING = ITEMS.register("pay_ging",
+                        () -> new Item(new Item.Properties().tab(PayGing.TAB)));
 
-    public static final RegistryObject<BlockItem> PAY_GING_BLOCK = ITEMS.register("pay_ging_block",
-            () -> new BlockItem(BlockInit.PAY_GING_BLOCK.get(),
-                    new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
+        public static final RegistryObject<BlockItem> PAY_GING_BLOCK = ITEMS.register("pay_ging_block",
+                        () -> new BlockItem(BlockInit.PAY_GING_BLOCK.get(), new Item.Properties().tab(PayGing.TAB)));
 
 }
